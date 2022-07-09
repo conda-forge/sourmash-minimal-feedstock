b8303bb2dcd8017ea4cf5137cc3e3389b71eedda
Luiz Irber <contact+github@luizirber.org>
2022-07-09 16:29:14 -0700

    update build

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,11 +15,12 @@ cargo build --release
 cp -a target/${CARGO_BUILD_TARGET}/release/libsourmash.${SOEXT} ${PREFIX}/lib/
 cp -a target/${CARGO_BUILD_TARGET}/release/libsourmash.a ${PREFIX}/lib/
 
-## This will work on currently unreleased sourmash, wait for 4.3.0+
 export NO_BUILD=1
 export DYLD_LIBRARY_PATH=${PREFIX}/lib
 
-$PYTHON -m pip install --no-deps --ignore-installed -vv .
+$PYTHON -m build --wheel --no-isolation -x
+$PYTHON -m build --sdist --no-isolation -x
+$PYTHON -m pip install --no-deps --no-index --only-binary sourmash --find-links=dist sourmash
 
 mkdir -p ${PREFIX}/lib/pkgconfig
 cat > ${PREFIX}/lib/pkgconfig/sourmash.pc <<"EOF"
@@ -30,7 +31,7 @@ libdir=${exec_prefix}/lib
 
 Name: sourmash
 Description: Compute MinHash signatures for nucleotide (DNA/RNA) and protein sequences.
-Version: 0.10.0
+Version: 0.11.0
 Cflags: -I${includedir}
 Libs: -L${libdir} -lsourmash
 EOF
