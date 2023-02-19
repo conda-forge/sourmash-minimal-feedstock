d98180c61a72afde21a86b1d25b3bff3ea8bd787
Luiz Irber <github@luizirber.org>
2023-02-18 20:24:09 -0800

    build updates for 4.7.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,20 @@
-:: install python package
-%PYTHON% -m build --wheel --no-isolation -x || goto :error
-%PYTHON% -m build --sdist --no-isolation -x || goto :error
-%PYTHON% -m pip install --no-deps --no-index --only-binary sourmash --find-links=dist sourmash || goto :error
+REM https://github.com/rust-lang/cargo/issues/10583#issuecomment-1129997984
+set CARGO_NET_GIT_FETCH_WITH_CLI=true
+REM Create temp folder
+mkdir tmpbuild_%PY_VER%
+set TEMP=%CD%\tmpbuild_%PY_VER%
+REM Bundle all downstream library licenses
+pushd src/core
+cargo-bundle-licenses ^
+    --format yaml ^
+    --output %SRC_DIR%\THIRDPARTY.yml ^
+    || goto :error
+popd
+REM Run the maturin build via pip
+set PYTHONUTF8=1
+set PYTHONIOENCODING="UTF-8"
+set TMPDIR=tmpbuild_%PY_VER%
+%PYTHON% -m pip install . -vv
 
 :: TODO: copy headers to includedir
 
