91716231a759fa9d8e6eefacc9fe8ee9a96ff7d6
C. Titus Brown <titus@idyll.org>
2025-01-15 07:02:54 -0800

    add original build.bat back in

---
+++ recipe/build.bat
@@ -0,0 +1,23 @@
+:: install python package
+set "RUSTFLAGS=-C codegen-units=4"
+maturin build -v --jobs 1 --release --strip --manylinux off --interpreter=%PYTHON%
+if errorlevel 1 exit 1
+
+FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set sourmash_wheel=%%i
+%PYTHON% -m pip install --ignore-installed --no-deps %sourmash_wheel% -vv
+if errorlevel 1 exit 1
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+:: TODO: copy headers to includedir
+
+:: TODO: cargo build for shared and static libraries
+:: TODO: copy libs to prefix/lib
+
+:: maybe TODO? pkgconfig
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
